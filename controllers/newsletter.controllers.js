3a409ce3bbce55e81bd771092a561fe078449421
ElvisKemoi <elviskemoi001@gmail.com>
2025-02-09 16:31:45 +0300

    Fixed the newsletter issue

--- controllers/newsletter.controllers.js
+++ controllers/newsletter.controllers.js
@@ -2,26 +2,31 @@ const Newsletter = require("../models/newsletter.model");
 
 const addPerson = async (req, res) => {
 	try {
-		const { email } = req.body;
+		const { newsletterEmail } = req.body;
+		const email = newsletterEmail;
 
 		if (!email) {
-			return res
-				.status(400)
-				.json({ success: false, error: "Email is required" });
+			return res.redirect("/");
+			// return res
+			// 	.status(400)
+			// 	.json({ success: false, error: "Email is required" });
 		}
 
 		const foundEmail = await Newsletter.findOne({ email });
 
 		if (foundEmail) {
-			return res.status(400).json({
-				success: false,
-				error: "Email already exists in the mailing list!",
-			});
+			return res.redirect("/");
+			// return res.status(400).json({
+			// 	success: false,
+			// 	error: "Email already exists in the mailing list!",
+			// });
 		}
 
 		const newEmail = await Newsletter.create({ email });
 
-		return res.status(201).json({ success: true, data: newEmail });
+		return res.status(201).redirect("/");
+
+		// return res.status(201).json({ success: true, data: newEmail });
 	} catch (error) {
 		return res.status(500).json({ success: false, error: error.message });
 	}
